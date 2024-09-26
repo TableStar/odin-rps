63f38a96882daf548ea696730c7c9a5e32717af3
TableStar <afraroad@gmail.com>
2024-09-26 23:39:41 +0700

    feat: ui and refactoring core func

--- rps.js
+++ rps.js
@@ -5,53 +5,68 @@ const getComputerChoice = () => {
   return choices[comRNG];
 };
 
+let humanScore = 0;
+let comScore = 0;
+
 const getHumanChoice = () => {
-  let playerChoice = prompt(`Input either Rock, Paper or Scissor`);
-  if (choices.includes(playerChoice.toLowerCase())) {
-    return playerChoice.toLowerCase();
-  } else {
-    alert(`Error Input is Wrong!\nPlease Enter the correct Input`);
-    return getHumanChoice();
+  // let playerChoice = prompt(`Input either Rock, Paper or Scissor`);
+  // if (choices.includes(playerChoice.toLowerCase())) {
+  //   return playerChoice.toLowerCase();
+  // } else {
+  //   alert(`Error Input is Wrong!\nPlease Enter the correct Input`);
+  //   return getHumanChoice();
+  // }
+};
+
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === computerChoice) {
+    alert("it ties");
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      comScore++;
+      alert("You lose! Paper beat Rock");
+    } else if (computerChoice === "scissor") {
+      humanScore++;
+      alert("You Win! Rock beat Scissor");
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "scissor") {
+      comScore++;
+      alert("You lose! Scissor beat Paper");
+    } else if (computerChoice === "rock") {
+      humanScore++;
+      alert("You Win! Paper beat Rock");
+    }
+  } else if (humanChoice === "scissor") {
+    if (computerChoice === "rock") {
+      comScore++;
+      alert("You lose! Rock Beat Scissor");
+    } else if (computerChoice === "paper") {
+      humanScore++;
+      alert("You Win! Scissor beat Paper");
+    }
   }
 };
 
-const playGame = () => {
-  let humanScore = 0;
-  let comScore = 0;
-  const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === computerChoice) {
-      alert("it ties");
-    } else if (humanChoice === "rock") {
-      if (computerChoice === "paper") {
-        comScore++;
-        alert("You lose! Paper beat Rock");
-      } else if (computerChoice === "scissor") {
-        humanScore++;
-        alert("You Win! Rock beat Scissor");
-      }
-    } else if (humanChoice === "paper") {
-      if (computerChoice === "scissor") {
-        comScore++;
-        alert("You lose! Scissor beat Paper");
-      } else if (computerChoice === "rock") {
-        humanScore++;
-        alert("You Win! Paper beat Rock");
-      }
-    } else if (humanChoice === "scissor") {
-      if (computerChoice === "rock") {
-        comScore++;
-        alert("You lose! Rock Beat Scissor");
-      } else if (computerChoice === "paper") {
-        humanScore++;
-        alert("You Win! Scissor beat Paper");
-      }
-    }
-  };
-  for (let i = 1; i <= 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-  }
-  alert(`The score is\nHuman:${humanScore}\nComputer:${comScore}`);
+const playGame = (button) => {
+  const humanChoice = button.id.toLowerCase();
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+
+  let score = document.querySelector(".score")
+  score.textContent = `Score\nHuman:${humanScore}\nComputer:${comScore}`;
+  
+  // // for (let i = 1; i <= 5; i++) {
+  // const humanChoice = getHumanChoice();
+  // const computerChoice = getComputerChoice();
+  // //   playRound(humanChoice, computerChoice);
+  // // }
+  // alert(`The score is\nHuman:${humanScore}\nComputer:${comScore}`);
 };
-playGame();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playGame(button);
+  });
+});
